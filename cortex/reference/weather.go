926821d062e3a710a698e468c86fa450093e816b
Dharun Ravi <d@ironbay.digital>
2015-03-19 23:20:52 -0400

    commented out weather

--- cortex/reference/weather.go
+++ cortex/reference/weather.go
@@ -1,10 +1,10 @@
 package reference
 
+/*
 import (
     "github.com/ironbay/snooze"
 )
 
-/*
 type weather struct {
     Get func(location string) (map[string]interface{}, error) `GET:/data/2.5/weather?q={0}`
 }
