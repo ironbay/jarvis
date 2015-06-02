9560e7341f83632a7b68f51a687f548f23e4dffc
Dax Ravi <d@ironbay.digital>
2015-06-02 00:17:53 +0000

    Ignore internal spanish and hebsub

--- media/torrentleech.irc.go
+++ media/torrentleech.irc.go
@@ -1,51 +1,56 @@
 package media
 
 import (
-	"regexp"
+    "regexp"
+    "strings"
 
-	irc "github.com/fluffle/goirc/client"
-	"github.com/ironbay/jarvis/cortex"
+    irc "github.com/fluffle/goirc/client"
+    "github.com/ironbay/jarvis/cortex"
 )
 
 type TorrentUpload struct {
-	Id       string
-	Name     string
-	Url      string
-	Category string
-	Size     string
+    Id       string
+    Name     string
+    Url      string
+    Category string
+    Size     string
 }
 
 func init() {
-	name := regexp.MustCompile("Name:'([^']+)")
-	category := regexp.MustCompile("<([^>]+)")
-	id := regexp.MustCompile("torrent/(\\d+)")
+    name := regexp.MustCompile("Name:'([^']+)")
+    category := regexp.MustCompile("<([^>]+)")
+    ignore := regexp.MustCompile("internal|spanish|hebsub")
+    id := regexp.MustCompile("torrent/(\\d+)")
 
-	cfg := irc.NewConfig("jarvis")
-	cfg.SSL = false
-	cfg.Server = "irc.torrentleech.org:7011"
-	cfg.NewNick = func(n string) string { return n + "^" }
-	c := irc.Client(cfg)
+    cfg := irc.NewConfig("jarvis")
+    cfg.SSL = false
+    cfg.Server = "irc.torrentleech.org:7011"
+    cfg.NewNick = func(n string) string { return n + "^" }
+    c := irc.Client(cfg)
 
-	c.HandleFunc("connected", func(conn *irc.Conn, line *irc.Line) {
-		cortex.Event.Message("Connected to Torrentleech", cortex.NoContext())
-		conn.Join("#tlannounces")
-	})
+    c.HandleFunc("connected", func(conn *irc.Conn, line *irc.Line) {
+        cortex.Event.Message("Connected to Torrentleech", cortex.NoContext())
+        conn.Join("#tlannounces")
+    })
 
-	c.HandleFunc(irc.PRIVMSG, func(conn *irc.Conn, line *irc.Line) {
-		text := line.Text()
-		model := new(TorrentUpload)
-		model.Name = name.FindStringSubmatch(text)[1]
-		model.Category = category.FindStringSubmatch(text)[1]
-		model.Id = id.FindStringSubmatch(text)[1]
-		model.Url = "http://www.torrentleech.org/rss/download/" + model.Id + "/" + Torrentleech.Key + "/download"
-		cortex.Event.Emit(model, cortex.NoContext())
-	})
+    c.HandleFunc(irc.PRIVMSG, func(conn *irc.Conn, line *irc.Line) {
+        text := line.Text()
+        model := new(TorrentUpload)
+        model.Name = name.FindStringSubmatch(text)[1]
+        if ignore.Match(strings.ToLower(model.Name)) {
+            return
+        }
+        model.Category = category.FindStringSubmatch(text)[1]
+        model.Id = id.FindStringSubmatch(text)[1]
+        model.Url = "http://www.torrentleech.org/rss/download/" + model.Id + "/" + Torrentleech.Key + "/download"
+        cortex.Event.Emit(model, cortex.NoContext())
+    })
 
-	c.HandleFunc("disconnected", func(conn *irc.Conn, line *irc.Line) {
-		cortex.Event.Error("Reconnecting to Torrentleech...", cortex.NoContext())
-		c.Connect()
-	})
+    c.HandleFunc("disconnected", func(conn *irc.Conn, line *irc.Line) {
+        cortex.Event.Error("Reconnecting to Torrentleech...", cortex.NoContext())
+        c.Connect()
+    })
 
-	c.Connect()
+    c.Connect()
 
 }
