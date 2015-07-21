32b67b2e89b0a065063a8f724fb0f9c442c92a52
Dax Ravi <d@ironbay.digital>
2015-07-21 05:45:42 +0000

    All TV and 720p

--- media/television.go
+++ media/television.go
@@ -1,57 +1,57 @@
 package media
 
 import (
-    "github.com/ironbay/jarvis/cortex"
-    "github.com/ironbay/jarvis/reference"
-    "strings"
+	"github.com/ironbay/jarvis/cortex"
+	"github.com/ironbay/jarvis/reference"
+	"strings"
 )
 
 type Show struct {
-    Name string
+	Name string
 }
 
 func (s *Show) Key() string {
-    return s.Name
+	return s.Name
 }
 
 func (s *Show) Alert() string {
-    return "Now following show: " + s.Name
+	return "Now following show: " + s.Name
 }
 
 func init() {
-    cortex.Event.Listen(func(model *TorrentUpload, context *cortex.Context) {
-        if model.Category != "TV :: Episodes" {
-            return
-        }
-        shows := make([]Show, 0)
-        cortex.Database.Get(&shows)
-        lower := strings.ToLower(model.Name)
-        for _, m := range shows {
-            if strings.Index(lower, strings.ToLower(m.Name)) == 0 {
-                m := TorrentStart{
-                    Url:  model.Url,
-                    Name: model.Name}
-                cortex.Event.Emit(&m, context)
-                return
-            }
-        }
-    })
+	cortex.Event.Listen(func(model *TorrentUpload, context *cortex.Context) {
+		if !strings.Contains(model.Category, "TV") {
+			return
+		}
+		shows := make([]Show, 0)
+		cortex.Database.Get(&shows)
+		lower := strings.ToLower(model.Name)
+		for _, m := range shows {
+			if strings.Index(lower, strings.ToLower(m.Name)) == 0 {
+				m := TorrentStart{
+					Url:  model.Url,
+					Name: model.Name}
+				cortex.Event.Emit(&m, context)
+				return
+			}
+		}
+	})
 
-    cortex.Event.Listen(func(model *Imdb, context *cortex.Context) {
-        if model.Type != "TV" {
-            return
-        }
-        r := Show{Name: model.Name}
-        cortex.Event.Emit(&r, context)
-    })
+	cortex.Event.Listen(func(model *Imdb, context *cortex.Context) {
+		if model.Type != "TV" {
+			return
+		}
+		r := Show{Name: model.Name}
+		cortex.Event.Emit(&r, context)
+	})
 
-    cortex.Pipe.Global("follow show (.+)", func(context *cortex.Context, args []string) {
-        r, _ := reference.Omdb.Search(args[1], "series")
-        if r["Response"].(string) != "True" {
-            return
-        }
-        m := Show{r["Title"].(string)}
-        cortex.Event.Emit(&m, context)
-    })
+	cortex.Pipe.Global("follow show (.+)", func(context *cortex.Context, args []string) {
+		r, _ := reference.Omdb.Search(args[1], "series")
+		if r["Response"].(string) != "True" {
+			return
+		}
+		m := Show{r["Title"].(string)}
+		cortex.Event.Emit(&m, context)
+	})
 
 }
