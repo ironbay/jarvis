615666aa9ae286d30faf623657ac9f050f8c0cc5
Dax <d@ironbay.digital>
2015-07-28 03:10:02 -0400

    Update omdb.go

--- cortex/reference/omdb.go
+++ cortex/reference/omdb.go
@@ -5,7 +5,7 @@ import (
 )
 
 type omdb struct {
-    Search func(title string, kind string) (map[string]interface{}, error) `GET:/?t={0}&type={1}`
+    Search func(title string, kind string) (map[string]interface{}, error) `method:"GET" path:"/?t={0}&type={1}"`
 }
 
 var Omdb = func() *omdb {
