96ac214db5ac6469dacd7dea51730a27cab09460
Dax Ravi <d@ironbay.digital>
2016-11-30 21:52:18 -0500

    Wait

--- lib/skill/media/download.ex
+++ lib/skill/media/download.ex
@@ -1,6 +1,6 @@
 defmodule Jarvis.Media.Download do
 	use Bot.Skill
-	@wait 0
+	@wait 1000 * 60
 	@key "c7afa073572a4ee09f8c"
 	@tv "/media/content/union/tv"
 	@downloaded "/media/torrents/downloaded"
