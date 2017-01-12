0742ead0e2d4b864cd11f06746a331df03910702
Dax Ravi <d@ironbay.digital>
2017-01-11 22:15:52 -0500

    Scan tv folder

--- lib/skill/media/download.ex
+++ lib/skill/media/download.ex
@@ -2,7 +2,7 @@ defmodule Jarvis.Media.Download do
 	use Bot.Skill
 	@wait 1000 * 60
 	@key "c7afa073572a4ee09f8c"
-	@tv "/media/content/union/tv"
+	@tv "/media/library/local/tv"
 	@downloaded "/media/torrents/downloaded"
 	@cookies "__cfduid=dd4e194047843c4d6a501c8927af3f85e1445145199; __utma=194598568.1862899498.1445145199.1454834546.1454879669.128; _ga=GA1.2.1862899498.1445145199; member_id=53563; pass_hash=ef5be2fa3121fe9947bbbf247bddbc99; session_id=f73f16f6fa47c606253ac227fc8d8a59; itemMarking_forums_items=eJxLtDK0qs60MjaxsLC0zrQyNDE3MbI0NgRyagFiwwbb; tluid=522483; tlpass=52e939c0ed7adbd2304b1b898867c8a5a2e6d56f; PHPSESSID=grq60nchbpp84jh5otcdq403a4; lastBrowse1=1475432959; lastBrowse2=1475450576"
 
@@ -30,14 +30,15 @@ defmodule Jarvis.Media.Download do
 	end
 
 	def handle_cast_async({"torrent.search", %{query: query}, context}, bot, data) do
-		options = "https://classic.torrentleech.org/torrents/browse/index/query/#{query}/categories/10%2C11%2C13%2C14%2C36%2C37%2C41%2C43%2C32/orderby/leechers/order/desc"
-		|> HTTPoison.get!(%{}, hackney: [cookie: [@cookies]])
-		|> Map.get(:body)
-		|> Floki.find("#torrenttable tbody tr")
-		|> Stream.map(&parse_row(&1))
-		|> Stream.with_index
-		|> Stream.take(3)
-		|> Enum.to_list
+		options =
+			"https://classic.torrentleech.org/torrents/browse/index/query/#{query}/categories/10%2C11%2C13%2C14%2C36%2C37%2C41%2C43%2C32/orderby/leechers/order/desc"
+			|> HTTPoison.get!(%{}, hackney: [cookie: [@cookies]])
+			|> Map.get(:body)
+			|> Floki.find("#torrenttable tbody tr")
+			|> Stream.map(&parse_row(&1))
+			|> Stream.with_index
+			|> Stream.take(3)
+			|> Enum.to_list
 
 		case Enum.count(options) do
 			0 ->
